2c201a1d43f2d70dfb604bcb2373c182621cd2be
aruntemme <arungauthamk@gmail.com>
2021-08-16 23:56:01 +0530

    responsice fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Button from './Button';
 function App() {
   return (
-    <div className="w-6/12 mx-auto">
+    <div className="w-10/12 sm:w-7/12 lg:w-6/12 mx-auto">
       <div>
         <input className="w-full border-2 rounded-lg text-3xl my-2 mt-10 border-purple-300 h-20" placeholder="enter"></input>
       </div>
